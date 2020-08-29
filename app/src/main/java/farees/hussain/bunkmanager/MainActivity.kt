9783597989e54bf109f48c38e70bac3490546595
Farees Hussain <fareezzhussain@gmail.com>
2020-08-29 23:09:24 +0530

    initial commit

---
+++ app/src/main/java/farees/hussain/bunkmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package farees.hussain.bunkmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
