4bafb58f8c72216121163c467c129538867a6b35
Farees Hussain <fareezzhussain@gmail.com>
2020-10-11 02:13:33 +0530

    added bindingAdapters in BindingUtils.kt

--- app/src/main/java/farees/hussain/bunkmanager/adapter/SubjectItemAdapter.kt
+++ app/src/main/java/farees/hussain/bunkmanager/adapter/SubjectItemAdapter.kt
@@ -16,65 +16,69 @@ class SubjectItemAdapter() : ListAdapter<Subject, SubjectItemAdapter.ClassHolder
 
     override fun onBindViewHolder(holder: ClassHolder, position: Int) {
         var subject = getItem(position)
-        subject.percentageAttendance = if(subject.totalClasses == 0) 0.0 else Math.round((subject.classesAttended.toDouble()*100/subject.totalClasses).toDouble() * 10.0)/10.0
-        holder.binding.tvPercent.text = "${subject.percentageAttendance}%"
-        holder.binding.tvSubject.text = subject.subjectName
-        holder.binding.tvCurrentAttendance.text = "Current Attendance : ${subject.currentAttendance}"
-
-        /*attendance status if less than 75 and if greater than 75%*/
-        fun changeStatus() {
-            if(subject.totalClasses == 0){
-                holder.binding.tvStatus.text = "Attendance Not Yet Started"
-                return
-            }
-            holder.binding.tvCurrentAttendance.text = "Current Attendance : ${subject.classesAttended}/${subject.totalClasses}"
-            if (subject.percentageAttendance!! < subject.requiredPercentageAttendance) {
-                var noOfClassesToAttend = 3 * subject.totalClasses - 4 * subject.classesAttended
-                if (noOfClassesToAttend < 0) noOfClassesToAttend++
-                subject.classesMustAttend = noOfClassesToAttend
-                holder.binding.tvStatus.text =
-                    "To get More Than ${subject.requiredPercentageAttendance}% Attend $noOfClassesToAttend classes"
-                holder.binding.tvStatus.setTextColor(Color.RED)
-            } else {
-                var noOfClassesCanBeBunked = 0
-                var a = subject.classesAttended
-                var t = subject.totalClasses
-                while(a*100/t >= subject.requiredPercentageAttendance.toDouble()){
-                    noOfClassesCanBeBunked++
-                    t++
-                }
-                if(a*100/t<subject.requiredPercentageAttendance)noOfClassesCanBeBunked--
-                subject.classesCanBeBunked = noOfClassesCanBeBunked
-                holder.binding.tvStatus.text = if(noOfClassesCanBeBunked>0) "You can now Bunk $noOfClassesCanBeBunked classes" else "You Can't Bunk any Class Now"
-                holder.binding.tvStatus.setTextColor(Color.MAGENTA)
-            }
-        }
-        changeStatus()
-        holder.binding.buBunk.setOnClickListener {
-            subject.totalClasses++
-            subject.currentAttendance = "Current Attendance ${subject.classesAttended}/${subject.totalClasses}"
-            var percentage = subject.classesAttended.toDouble() * 100.0/subject.totalClasses
-            subject.percentageAttendance = Math.round(percentage * 10.0)/10.0
-
-            holder.binding.tvCurrentAttendance.text = "Current Attendance : ${subject.classesAttended}/${subject.totalClasses}"
-            holder.binding.tvPercent.text = "${subject.percentageAttendance}%"
-            changeStatus()
-        }
-        holder.binding.buAttend.setOnClickListener {
-            subject.classesAttended++
-            subject.totalClasses++
-            subject.currentAttendance = "Current Attendance : ${subject.classesAttended}/${subject.totalClasses}"
-            var percentage = subject.classesAttended.toDouble() * 100.0/subject.totalClasses
-            subject.percentageAttendance = Math.round(percentage * 10.0)/10.0
-
-            holder.binding.tvCurrentAttendance.text = subject.currentAttendance
-            holder.binding.tvPercent.text = "${subject.percentageAttendance}%"
-            changeStatus()
-        }
+        holder.bind(subject)
     }
 
 
-    inner class ClassHolder(val binding: ItemSubjectsBinding):RecyclerView.ViewHolder(binding.root)
+    inner class ClassHolder(val binding: ItemSubjectsBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(item: Subject){
+            item.percentageAttendance = if(item.totalClasses == 0) 0.0 else Math.round((item.classesAttended.toDouble()*100/item.totalClasses).toDouble() * 10.0)/10.0
+            binding.subject = item
+            binding.executePendingBindings()
+
+            /*attendance status if less than 75 and if greater than 75%*/
+            fun changeStatus() {
+                if(item.totalClasses == 0){
+                    binding.tvStatus.text = "Attendance Not Yet Started"
+                    return
+                }
+                binding.tvCurrentAttendance.text = "Current Attendance : ${item.classesAttended}/${item.totalClasses}"
+                if (item.percentageAttendance!! < item.requiredPercentageAttendance) {
+                    var noOfClassesToAttend = 3 * item.totalClasses - 4 * item.classesAttended
+                    if (noOfClassesToAttend < 0) noOfClassesToAttend++
+                    item.classesMustAttend = noOfClassesToAttend
+                    binding.tvStatus.text =
+                        "To get More Than ${item.requiredPercentageAttendance}% Attend $noOfClassesToAttend classes"
+                    binding.tvStatus.setTextColor(Color.RED)
+                } else {
+                    var noOfClassesCanBeBunked = 0
+                    var a = item.classesAttended
+                    var t = item.totalClasses
+                    while(a*100/t >= item.requiredPercentageAttendance.toDouble()){
+                        noOfClassesCanBeBunked++
+                        t++
+                    }
+                    if(a*100/t<item.requiredPercentageAttendance)noOfClassesCanBeBunked--
+                    item.classesCanBeBunked = noOfClassesCanBeBunked
+                    binding.tvStatus.text = if(noOfClassesCanBeBunked>0) "You can now Bunk $noOfClassesCanBeBunked classes" else "You Can't Bunk any Class Now"
+                    binding.tvStatus.setTextColor(Color.MAGENTA)
+                }
+            }
+            changeStatus()
+            binding.buBunk.setOnClickListener {
+                item.totalClasses++
+                item.currentAttendance = "Current Attendance ${item.classesAttended}/${item.totalClasses}"
+                var percentage = item.classesAttended.toDouble() * 100.0/item.totalClasses
+                item.percentageAttendance = Math.round(percentage * 10.0)/10.0
+
+                binding.tvCurrentAttendance.text = "Current Attendance : ${item.classesAttended}/${item.totalClasses}"
+//                binding.tvPercent.text = "${item.percentageAttendance}%"
+                changeStatus()
+            }
+            binding.buAttend.setOnClickListener {
+                item.classesAttended++
+                item.totalClasses++
+                item.currentAttendance = "Current Attendance : ${item.classesAttended}/${item.totalClasses}"
+                var percentage = item.classesAttended.toDouble() * 100.0/item.totalClasses
+                item.percentageAttendance = Math.round(percentage * 10.0)/10.0
+
+                binding.tvCurrentAttendance.text = item.currentAttendance
+//                binding.tvPercent.text = "${item.percentageAttendance}%"
+                changeStatus()
+                binding.subject = item
+            }
+        }
+    }
 }
 
 class SubjectItemDiffCallBack : DiffUtil.ItemCallback<Subject>(){
--- app/src/main/java/farees/hussain/bunkmanager/utils/BindingUtils.kt
+++ app/src/main/java/farees/hussain/bunkmanager/utils/BindingUtils.kt
@@ -1,5 +1,22 @@
 package farees.hussain.bunkmanager.utils
 
-class BindingUtils {
-    //todo add binding adapters to the subjectItems
+import android.annotation.SuppressLint
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import farees.hussain.bunkmanager.R
+import farees.hussain.bunkmanager.db.model.Subject
+
+//todo add binding adapters to the subjectItems
+@BindingAdapter("subjectPercentageAttendance")
+fun TextView.setAttendancPercentage(item: Subject){
+    text = "${item.percentageAttendance}%"
+}
+@BindingAdapter("subjectName")
+fun TextView.setSubjectName(item:Subject){
+    text = item.subjectName
+}
+@BindingAdapter("currentAttendance")
+fun TextView.setCurrentAttendance(item: Subject){
+    text = item.currentAttendance
+    setTextColor(resources.getColor(R.color.canBunk))
 }
